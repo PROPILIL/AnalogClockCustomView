d51c02cb330b2a7056fb66959c5227357eaeb50c
PROPILIL <83790466+PROPILIL@users.noreply.github.com>
2022-08-16 01:49:28 +0300

    There's no commits in project, because of gradle crush. This one - just a new one project with copied and refactored code from the broken one =/

---
+++ app/src/main/java/com/propil/clockview/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.propil.clockview
+
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.propil.clockview.views.AnalogClock
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
